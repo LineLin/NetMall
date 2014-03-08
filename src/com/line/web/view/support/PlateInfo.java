0f0b12f5725186cd4bcbbb3cc8cb65bf198c7607
Line <whlin926@gmail.com>
2014-03-08 16:55:42 +0800

    新建PlateInfo类。以及调制了测试包的结构

---
+++ src/com/line/web/view/support/PlateInfo.java
@@ -0,0 +1,38 @@
+package com.line.web.view.support;
+
+import java.util.List;
+import com.line.web.model.Plate;
+
+public class PlateInfo {
+	
+	private Plate plate;
+	
+	private String linkPrefix;
+	
+	private List<PlateInfo> subPlate;
+
+	public Plate getPlate() {
+		return plate;
+	}
+
+	public void setPlate(Plate plate) {
+		this.plate = plate;
+	}
+
+	public String getLinkPrefix() {
+		return linkPrefix;
+	}
+
+	public void setLinkPrefix(String linkPrefix) {
+		this.linkPrefix = linkPrefix;
+	}
+
+	public List<PlateInfo> getSubPlate() {
+		return subPlate;
+	}
+
+	public void setSubPlate(List<PlateInfo> subPlate) {
+		this.subPlate = subPlate;
+	}	
+	
+}
